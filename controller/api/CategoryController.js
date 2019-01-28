67e69ed1278b2fa04c1c0c47402bfde4ba6531c0
Tanya Reznikova <tanya.reznikova375@gmail.com>
2019-01-28 22:03:34 +0300

    + was added some change +

--- controller/api/CategoryController.js
+++ controller/api/CategoryController.js
@@ -1,1 +1,31 @@
-//
+var mongoose = require("mongoose");
+var Category = mongoose.model("Category");
+
+var categoryController = {};
+
+//Add show list of category function
+categoryController.list = function(req, res) {
+    Category.find({}).exec(function (err, categories) {
+        if (err) {
+            console.log("Error:", err);
+        }
+        else {
+            //ИЗМЕНИТЬ
+            res.render("../views/employees/index", {categories: categories});
+        }
+    });
+};
+
+//Add show single category by id function
+categoryController.show = function(req, res) {
+    Category.findOne({_id: req.params.id}).exec(function (err, categories) {
+        if (err) {
+            console.log("Error:", err);
+        }
+        else {
+            //ИЗМЕНИТЬ
+            res.render("../views/employees/show", {categories: categories});
+        }
+    });
+};
+
